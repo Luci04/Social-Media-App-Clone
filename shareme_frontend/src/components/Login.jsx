3078e1a9413237497e7d5af0d040dc282b18496a
luci04 <avinashukla2000@gmail.com>
2022-01-02 00:03:42 +0530

    Front-end Setip

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc'
+import shareVideo from '../assets/share.mp4';
+import Logo from '../assets/logowhite.png';
+
+export default function Login() {
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
