5e53253a24e566e8637ee0a8b2def2737949def5
luci04 <avinashukla2000@gmail.com>
2022-01-02 23:54:29 +0530

    Navbar styling Done

--- shareme_frontend/src/container/Pins.jsx
+++ shareme_frontend/src/container/Pins.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Feed from "../components/Feed";
+import PinDetail from "../components/PinDetail";
+import CreatePin from "../components/CreatePin";
+import Search from "../components/Search";
 
-const Pins = () => {
-  return <div>Pins</div>;
+const Pins = ({ user }) => {
+  const [searchTerm, setsearchTerm] = useState("");
+
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar
+          user={user}
+          searchTerm={searchTerm}
+          setsearchTerm={setsearchTerm}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user} />}
+          />
+          <Route path="/create-pin" element={<CreatePin />} />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
